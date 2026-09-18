b8104837d3efa8f11056b1a0cb8290267154b6ac
R216YU <r216yu@gmail.com>
2026-09-18 16:34:13 +0900

    init page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,12 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from "@tanstack/react-router";
 
-export const Route = createFileRoute('/')({ component: Home })
+export const Route = createFileRoute("/")({ component: Home });
 
 function Home() {
   return (
     <div className="p-8">
-      <h1 className="text-4xl font-bold">Welcome to TanStack Start</h1>
-      <p className="mt-4 text-lg">
-        Edit <code>src/routes/index.tsx</code> to get started.
-      </p>
+      <h1 className="text-xl text-center">r216yu.xyz</h1>
+      <p className="text-center">Building now...</p>
     </div>
-  )
+  );
 }
